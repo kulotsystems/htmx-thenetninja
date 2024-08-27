cd7e9e2090137832d3aabe345a038aa62cad97c0
Arvic S. Babol <kulotsystems@gmail.com>
2024-08-27 14:06:43 +0800

    Lesson 6: Adding Redirects

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import createHomepageTemplate from './views/index.js';
 import createListTemplate from './views/list.js';
+import createBookTemplate from "./views/book.js";
 import BOOKS_DATA from './data/data.js';
 
 // create app
@@ -24,7 +25,14 @@ app.post('/books', (req, res) => {
     const id = Math.random().toString();
 
     BOOKS_DATA.push({ id, title, author });
-    res.send(`<li>${title}, ${author}</li>`);
+    res.redirect(`/books/${id}`);
+});
+
+app.get('/books/:id', (req, res) => {
+    const { id } = req.params;
+    const book = BOOKS_DATA.find((b) => b.id === id);
+
+    res.send(createBookTemplate(book));
 });
 
 // listen to port
